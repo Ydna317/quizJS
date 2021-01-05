3543f9883f53da82df202fa3a7d2e9fcea23b42c
Andy Nguyen <Ydna317@gmail.com>
2021-01-04 17:53:36 -0800

    Adding resources and finalizing project

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+  {
+    title: "Which of these is not a coding language?",
+    choices: ["CSS", "JS", "HTML", "WTF"],
+    answer: "WTF"
+  },
+  {
+    title: "What does HTML stand for?",
+    choices: ["Hypertext Markup Language", "Highly Targeted Manipulative Language", "Hold The Man's Leg", "Have To Make Love"],
+    answer: "Hypertext Markup Language"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
